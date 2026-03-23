741e0769f9bc7f99cc9f2e0525264e7d6a04cd09
MB <mbteamai@gmail.com>
2026-03-23 17:48:21 +0800

    Add Hello World program

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def hello_world():
+    """打印 Hello World 消息"""
+    print("Hello, World!")
+    print("你好，世界！")
+
+if __name__ == "__main__":
+    hello_world()
